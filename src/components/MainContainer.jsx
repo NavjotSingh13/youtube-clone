59e67123b83365826615a03b2d19ae7e2faa041b
Singhnav13 <navjot131@gmail.com>
2023-08-12 20:48:29 +0530

    JS to JSX

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,7 +4,7 @@ import VideoContainer from './VideoContainer';
 
 const MainContainer = () => {
   return (
-    <div>
+    <div className="col-span-10">
         <Filters />
         <VideoContainer />
     </div>

5cd5289556374afa38dfb81fe14bf06094504790
Singhnav13 <navjot131@gmail.com>
2023-08-15 13:56:16 +0530

    API call in progress

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { YOUTUBE_API_key } from './utils/constants';
 
 const VideoContainer = () => {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+
+  }, []);  
+
+
+
+   const fetchData = async () => {
+     const data = await fetch(
+       'https://api.publicapis.org/entries'
+     );
+ 
+    
+
+     const json = await data.json();
+     setVideos (json.items[0]);
+     console.log(json);
+   };
+
+
+
+  
+
+
   return (
-    <div>VideoContainer</div>
+    <div>
+      {videos.map ((dataObj) => {
+        return (
+          <p>{dataObj.items}</p>
+        )
+      })}
+    </div>
   )
 }
 

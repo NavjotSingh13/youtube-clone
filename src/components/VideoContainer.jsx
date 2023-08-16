0e8a2601cc3652531203232a766c209dec0c7d78
Singhnav13 <navjot131@gmail.com>
2023-08-16 15:56:39 +0530

    Designing VideoContainer

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -2,42 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { YOUTUBE_API_key } from './utils/constants';
 
 const VideoContainer = () => {
-
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchData();
-
-  }, []);  
-
-
-
-   const fetchData = async () => {
-     const data = await fetch(
-       'https://api.publicapis.org/entries'
-     );
- 
-    
-
-     const json = await data.json();
-     setVideos (json.items[0]);
-     console.log(json);
-   };
-
-
-
-  
-
+    fetch(YOUTUBE_API_key)
+      .then(response => response.json())
+      .then(data => {
+        setVideos(data.items);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
-    <div>
-      {videos.map ((dataObj) => {
-        return (
-          <p>{dataObj.items}</p>
-        )
-      })}
+    <div className="p-12">
+      <h1>YouTube Videos</h1>
+      <ul className="flex">
+        {videos.map(video => (
+          <li className="red" key={video.id.videoId}>
+            <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+            <h3 className="text-orange-200">{video.snippet.title}</h3>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default VideoContainer;
